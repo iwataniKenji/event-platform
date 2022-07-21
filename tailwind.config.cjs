629945620f0f3cb4e764c25143df95aaf036f7b5
Kleverson Kenji <kleversonkenji@gmail.com>
2022-07-20 23:24:35 -0300

    setting application

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // seleciona todos os arquivos que terão estilização
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
