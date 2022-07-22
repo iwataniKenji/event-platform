7f42fbe5137d51d6482d31a40f64ac49030d9814
Kleverson Kenji <kleversonkenji@gmail.com>
2022-07-21 21:53:33 -0300

    formatting date with date-fns

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,6 @@
 import { Lock, CheckCircle } from "phosphor-react";
-import { isPast } from "date-fns";
+import { isPast, format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 interface LessonProps {
   title: string;
@@ -10,10 +11,17 @@ interface LessonProps {
 
 export function Lesson(props: LessonProps) {
   const isLessonAvailable = isPast(props.availableAt);
+  const availableDataFormatted = format(
+    props.availableAt,
+    "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
+    {
+      locale: ptBR,
+    }
+  );
 
   return (
     <a href="#">
-      <span className="text-gray-300">{props.availableAt.toString()}</span>
+      <span className="text-gray-300">{availableDataFormatted}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
